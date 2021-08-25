ca9ed94a6120e43c14d3ce016071fefaedca79a4
Enson Choy <enson.choy@harmonicinc.com>
2021-08-25 15:21:11 +0800

    Fix: Discard only invalid overlapping periods
    Discard only invalid and/or overlapping periods to fulfil the requirements

--- test/dash/dash_parser_manifest_unit.js
+++ test/dash/dash_parser_manifest_unit.js
@@ -2194,7 +2194,8 @@ describe('DashParser Manifest', () => {
   it('skip periods that are earlier than max period start time', async () => {
     const sources = [
       buildManifestWithPeriodStartTime([5, 15]),
-      buildManifestWithPeriodStartTime([4, 15]), // simulate out-of-sync of -1s
+      buildManifestWithPeriodStartTime([6, 15]), // simulate out-of-sync of -1s
+      buildManifestWithPeriodStartTime([4, 15]), // simulate out-of-sync of +1s
     ];
     const segments = [];
 
